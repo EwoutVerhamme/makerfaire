cbb380c3a2d9ed1ce5460c9e34321bf67f144ead
Your Name HereEwout <ewout.verhamme@student.howest.be>
2021-01-17 16:10:54 +0100

    Made first header

--- src/view/order/index.php
+++ src/view/order/index.php
@@ -1,1 +1,16 @@
-<h1 class="title">Dit is de homepage</h1>
+<header class="header">
+<nav class="nav">
+<ul class="menu">
+<img src="../../assets/img/home/logo_nav.svg" alt="Maker Faire navigation logo" class="nav__img">
+<div class="menu__items">
+<li class="menu__item">Maker Faire</li>
+<li class="menu__item">DIY-pakket</li>
+<li class="menu__item">Tutorial</li>
+<li class="menu__item">NL/FR</li>
+</div>
+
+</ul>
+</nav>
+
+
+</header>
