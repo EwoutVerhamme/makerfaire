86d3865353f023b2318f0ffea4c6e64a67b1409f
Your Name HereEwout <ewout.verhamme@student.howest.be>
2021-01-19 09:08:33 +0100

    homepage development

--- src/view/layout.php
+++ src/view/layout.php
@@ -1,32 +1,28 @@
 <!DOCTYPE html>
 <html lang="nl-BE">
-  <head>
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
-    <meta name="description" content="Alles over de kampioenen">
+    <meta name="description" content="Maker Faire">
 
-    <title>Maker Faire -  <?php echo $title; ?></title>
+    <title>Maker Faire - <?php echo $title; ?></title>
     <?php echo $css;?>
-  </head>
-  <body>
-  <div style="display: flex; align-items: center; justify-content: space-between; background: black; color: white; position: fixed; top: 0; left: 0; width: 100%; padding: 0.5em; font-size: 12px; font-family: sans-serif;"><span>This is a fictional project, a student assignment for the Bachelor's degree <a href="https://devine.be" style="color: white;">Devine</a></span><button style="border: 0; padding: 0; background: black; color: white; margin: 0 0.5em; padding: 0 0.5em; cursor: pointer; font-size: 12px; " onclick="(function(btn){btn.parentNode.style.display='none'; localStorage.setItem('hide-devine-student-project-banner', '1'); return false;})(this);return false;">x</button><script>if (localStorage.getItem('hide-devine-student-project-banner') === '1') document.currentScript.parentNode.style.display = 'none';</script></div>
+</head>
 
-  <?php if (!empty($_SESSION['info'])): ?>
-          <div class="box info"><?php echo $_SESSION['info']; ?></div>
-        <?php endif; ?>
-        <?php if (!empty($_SESSION['error'])): ?>
-          <div class="box error"><?php echo $_SESSION['error']; ?></div>
-        <?php endif; ?>
+<body>
+    <!-- <div style="display: flex; align-items: center; justify-content: space-between; background: black; color: white; position: fixed; top: 0; left: 0; width: 100%; padding: 0.5em; font-size: 12px; font-family: sans-serif;"><span>This is a fictional project, a student assignment for the Bachelor's degree <a href="https://devine.be" style="color: white;">Devine</a></span><button style="border: 0; padding: 0; background: black; color: white; margin: 0 0.5em; padding: 0 0.5em; cursor: pointer; font-size: 12px; " onclick="(function(btn){btn.parentNode.style.display='none'; localStorage.setItem('hide-devine-student-project-banner', '1'); return false;})(this);return false;">x</button><script>if (localStorage.getItem('hide-devine-student-project-banner') === '1') document.currentScript.parentNode.style.display = 'none';</script></div> -->
 
-        <?php echo $content; ?>
-  
+    <?php if (!empty($_SESSION['info'])): ?>
+    <div class="box info"><?php echo $_SESSION['info']; ?></div>
+    <?php endif; ?>
+    <?php if (!empty($_SESSION['error'])): ?>
+    <div class="box error"><?php echo $_SESSION['error']; ?></div>
+    <?php endif; ?>
+
+    <?php echo $content; ?>
+
     <?php echo $js; ?>
-  </body>
+</body>
+
 </html>
--- src/view/order/home.php
+++ src/view/order/home.php
@@ -1,13 +1,38 @@
+<!-- <nav class="nav">
+    <div class="menu">
+        <img src="../../assets/img/home/logo_nav.svg" alt="Maker Faire navigation logo" class="nav__img">
+        <ul class="menu__items">
+            <li class="menu__item"><a class="menu__item" href="#">Maker Faire</a></li>
+            <li class="menu__item"><a class="menu__item" href="#">DIY-pakket</a></li>
+            <li class="highlight menu__item"><a class="menu__item" href="#">Tutorial</a></li>
+            <li class="menu__item"><a class="menu__item" href="#">NL/FR</a></li>
+        </ul>
+    </div>
+</nav> -->
+
 <header class="header">
-<nav class="nav">
-<div class="menu">
-<img src="../../assets/img/home/logo_nav.svg" alt="Maker Faire navigation logo" class="nav__img">
-<ul class="menu__items">
-<li class="menu__item"><a class="menu__item" href="#">Maker Faire</a></li>
-<li class="menu__item"><a class="menu__item" href="#">DIY-pakket</a></li>
-<li class="menu__item highlight"><a class="menu__item" href="#">Tutorial</a></li>
-<li class="menu__item"><a class="menu__item" href="#">NL/FR</a></li>
-</ul>
-</div>
-</nav>
+    <div class="header__info">
+        <h1 class="header__title">Maak jij de snelste drag-racewagen?</h1>
+        <p class="header__subtext subtext">
+            Kom racen met je eigen gebouwde racewagen op de <br>
+            Maker Faire <span class="span">30 april, 1 & 2 mei 2021! </span></p>
+        <div class="button__wrapper">
+            <button class="button">Naar de tutorial</button>
+        </div>
+    </div>
 </header>
+<div class="info">
+    <div class="info__left">
+        <img src="../../assets/img/home/logo.png" alt="" class="info__left--img">
+    </div>
+    <div class="info__right">
+        <h1 class="info__title">
+            Maker faire 2021
+        </h1>
+        <p class="info__subtext subtext">Blokkeer alvast 30 april, 1 & 2 mei 2021 in je agenda.
+            Want dan gaat de tweede editie van de Makers Faire door in de Chinastraat, een industriële site in hartje
+            Gent.</p>
+        <a href="" target="_blank" class="button">Website Maker Faire</a>
+    </div>
+
+</div>
