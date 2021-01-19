6171366ccd051a01684e30e31f8b38c7c82f7089
Your Name HereEwout <ewout.verhamme@student.howest.be>
2021-01-19 10:38:38 +0100

    Homepage responsive

--- src/view/order/home.php
+++ src/view/order/home.php
@@ -14,7 +14,7 @@
     <div class="header__info">
         <h1 class="header__title">Maak jij de snelste drag-racewagen?</h1>
         <p class="header__subtext subtext">
-            Kom racen met je eigen gebouwde racewagen op de <br>
+            Kom racen met je eigen gebouwde racewagen op de
             Maker Faire <span class="span">30 april, 1 & 2 mei 2021! </span></p>
         <div class="button__wrapper">
             <button class="button">Naar de tutorial</button>
@@ -27,12 +27,50 @@
     </div>
     <div class="info__right">
         <h1 class="info__title">
-            Maker faire 2021
+            Maker Faire 2021
         </h1>
         <p class="info__subtext subtext">Blokkeer alvast 30 april, 1 & 2 mei 2021 in je agenda.
             Want dan gaat de tweede editie van de Makers Faire door in de Chinastraat, een industriële site in hartje
             Gent.</p>
-        <a href="" target="_blank" class="button">Website Maker Faire</a>
+        <div class="button__wrapper">
+            <a href="" target="_blank"><button class="button">Website Maker Faire</button></a>
+        </div>
     </div>
-
 </div>
+
+<div class="numbers">
+    <div class="number__box">
+        <p class="number__text">20.403</p>
+        <p class="number__name">Bezoekers</p>
+    </div>
+    <div class="number__box">
+        <p class="number__text">94</p>
+        <p class="number__name">Vrijwilligers</p>
+    </div>
+    <div class="number__box">
+        <p class="number__text">1.034</p>
+        <p class="number__name">Makers</p>
+    </div>
+    <div class="number__box">
+        <p class="number__text">103</p>
+        <p class="number__name">Experimenten</p>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="sponsors">
+        <img class="sponsor__img" height="20" src="../../assets/img/sponsors/logo_accent.png" alt="">
+        <img class="sponsor__img" height="50" src="../../assets/img/sponsors/logo_vlaanderen.png" alt="">
+        <img class="sponsor__img" height="50" src="../../assets/img/sponsors/logo_gent.png" alt="">
+        <img class="sponsor__img" class="sponsor__img" height="50" src="../../assets/img/sponsors/logo_makezine.png"
+            alt="">
+        <img class="sponsor__img" height="50" src="../../assets/img/sponsors/logo_tentzetter.png" alt="">
+        <img class="sponsor__img" height="100" src="../../assets/img/sponsors/logo_ugent.png" alt="">
+        <img class="sponsor__img" height="100" src="../../assets/img/sponsors/logo_velleman.png" alt="">
+    </div>
+    <div class="footer__bottom--text">
+        <p class="footer__text">Maker Faire Gent is independently organized and operated under license from Maker Media,
+            Inc.</p>
+        <p class="footer__text"><span class="span">© 2020 MAKE IN BELGIUM</span></p>
+    </div>
+</footer>
