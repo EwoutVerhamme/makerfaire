d54dd3bce30945599bef53846c6b233303bb7b5a
Your Name HereEwout <ewout.verhamme@student.howest.be>
2021-01-21 14:37:05 +0100

    You can refresh in tutorial and not loosing you current page

--- src/js/tutorial.js
+++ src/js/tutorial.js
@@ -1,7 +1,11 @@
 {
+
+  let stepIndex = undefined;
+
     // Selectors
   const navigation = document.querySelector('.nav');
-  const tutorialButton = document.querySelectorAll('.tutorial__button');
+  const tutorialIntroButton = document.querySelectorAll('.intro__tutorial--button');
+  const tutorialButton = document.querySelectorAll('.tutorial__button')
   const header = document.querySelector('.header')
   
   // SELECT THE POPUP TO CHANCE HTML
@@ -16,24 +20,22 @@
   
   // SELECT ALL THE STEPS
   const stepsWrapper = document.querySelector('.steps__wrapper')
-  const step1 = document.querySelector('.step__1');
-  const step2 = document.querySelector('.step__2');
-  const step3 = document.querySelector('.step__3');
-  const step4 = document.querySelector('.step__4');
   const back = document.querySelector('.back');
 
-  
   // BEGIN TUTORIAL CODE
-  tutorialButton.forEach(button => {
+  tutorialIntroButton.forEach(button => {
     button.addEventListener('click', (e) => {
       switch(e.target.id) {
-        case "0":
+        case "start":
+          // Blur the header
           header.style.filter =  "blur(8px)";
+          // Bring popup to front
           popup.style.zIndex = "5";
+          // Add clean animation
           popup.style.animation = "fadein 2s"
           break;
           case "material":
-            // SET NEW ID FOR BUTTON
+            // Set new id for the button
             popupButton2.setAttribute("id", "dontwant" )
             window.location = "index.php?page=kit";
             break;
@@ -51,9 +53,9 @@
               // REMOVE POPUP, NAV, LEVEL AND GO TO STEP 1
               popup.style.display = "none";
               header.style.display =  "none";
+              stepsWrapper.style.zIndex = "2"
               stepsWrapper.classList.remove('hidden');
-              stepsWrapper.style.zIndex = "6"
-              step1.classList.remove('hidden');
+              renderStep(0)
               break;
             case "want":
             // UPDATE THE POPUP
@@ -62,30 +64,99 @@
             handtrack.style.display = "flex"
             noHandTrack.innerHTML = "Ik wil toch liever klikken"
             popupButtons.style.display = "none";
-            
             break;
-        case "1":
-          step1.classList.add("hidden");
-          step2.classList.remove('hidden');
-          break;
-          case "2":
-            step2.classList.add("hidden");
-            step3.classList.remove('hidden');
-          break;
-          case "3":
-            step3.classList.add("hidden");
-            step4.classList.remove('hidden');
-          break;
         default:
           console.log("Error")
       }
     })
   });
+
+// Select all the step-info
+const stepTitle = document.querySelector('.step__title');
+const stepText = document.querySelector('.step__subtext');
+const stepImg = document.querySelector('.step__right--img');
+
+// Get over all the buttons
+  tutorialButton.forEach(button => {
+    button.addEventListener('click', (e) => {
+      switch(e.target.id) {
+        case "btnStep0":
+          // Set Step 2
+          renderStep(1)
+          break;
+          case "btnStep1":
+            // Set step 3
+            renderStep(2)
+          break;
+          case "btnStep2":
+            // Set step 4
+            renderStep(3)
+          break;
+          case "btnStep3":
+            // Set step 5
+            renderStep(4)
+          break;
+          case "btnStep4":
+            // Set step 5
+            renderStep(5)
+          break;
+        default:
+          console.log("ErrorButtonTutorial")
+      }
+    })
+  });
+
+    const renderStep = (stepIndex) => {
+      // If user is going back in the browser 
+      window.onhashchange = () => {
+        stepIndex -1;
+      }
+      // Always set the popups and header to none
+      if(popup){
+        popup.style.display = "none";
+        header.style.display =  "none";
+        // Always set the steps to view
+        stepsWrapper.style.zIndex = "2"
+        stepsWrapper.classList.remove('hidden');
+      }
+  
+      // Selectors
+      const text = ["Elke maker begint zijn avontuur met schetsen. Zodat je zeker weet welke materialen en componenten je nodig hebt. Maak daarom een goede schets. Een top, front & side view zijn hierbij zeker niet overbodig.","Het eerste en ook het belangrijkste deel van de drag racewagen is de body. Dit deel houdt alles stevig bij elkaar. Je kan het in verschillende maten en figuren maken, zolang hij maar tegen een stootje kan.","Om je racewagen vooruit te helpen is een aandrijver een belangrijk onderdeel. Dit kan alles zijn. Een mixer, een motor van een speelgoed auto, boormachine. Het is belangrijk om hiervoor de juiste aandrijving te kiezen en goed vooraf te denken hoe je iets zal monteren met de meegeleverde klemmen.","Zonder de wielen kan je natuurlijk niet rijden. Bevestig de wielen aan je wielas met behulp van de meegeleverde connectie stukken. Draai de wielen stevig aan op de wielas om de speling zo klein mogelijk te houden. ","Nu je het basis-model hebt, is het belangrijk om hier je eigen creativiteit aan te geven. Ga opzoek naar gekke dingen die je kan toevoegen aan de racewagen. Maar zorg hierbij ook dat je geen snelheid verliest! "]
+    
+      // Get all the tutorial buttons
+      tutorialButton.forEach(button => {
+      button.setAttribute("id", `btnStep${stepIndex}` )
+      stepTitle.setAttribute("src", `../../assets/img/tutorial/step${stepIndex}/title.svg` )
+      stepImg.setAttribute("src", `../../assets/img/tutorial/step${stepIndex}/image.svg`)
+      stepText.textContent = text[stepIndex];
+      localStorage.setItem('step', stepIndex)
+  
+      // Set all the indicators colored
+      for(let i = 0; i <= stepIndex; i++){
+        // Set all the passed indicators to passed
+        const indicatorActive = document.querySelector(`.indicator${i}`)
+        indicatorActive.classList.add("step__indicator--active");
+      }
+      });      
+    }
   
   
-  const init =  () => {
+
+  const init = () => {
+    // Set the previous step after refresh
+      if(localStorage.getItem("step")){
+        const stepIndex = localStorage.getItem("step")
+        // Get the saved step 
+        renderStep(stepIndex)
+      }
   }
-  
-    init();
+
+  // Check if back exists to prevent error in console
+  if(back){
+    back.addEventListener('click', () => {
+      localStorage.removeItem('step')
+      console.log(localStorage)
+    })
   }
-  
+init()
+}
--- src/view/order/tutorial.php
+++ src/view/order/tutorial.php
@@ -9,8 +9,9 @@
                 dolore magna aliqua. Ut enim ad minim veniam.
             </p>
             <div class="popup__buttons">
-                <button id="material" class="button popup__button tutorial__button button1">Welk materiaal?</button>
-                <button id="letsgo" class="button tutorial__button button2">Ja, let's begin!</button>
+                <button id="material" class="button popup__button intro__tutorial--button button1">Welk
+                    materiaal?</button>
+                <button id="letsgo" class="button intro__tutorial--button button2">Ja, let's begin!</button>
             </div>
             <p class="handtrack">Handtracking laden...</p>
             <p class="nohandtrack"></p>
@@ -69,7 +70,7 @@
                 class="span">alles
                 aandachtig te lezen</span> en er je eigen inbreng aan te geven! </p>
         <div class="button__wrapper">
-            <button id="0" class="button tutorial__button">Start de tutorial</button>
+            <button id="start" class="button intro__tutorial--button">Start de tutorial</button>
         </div>
         <div class="indicators">
             <div class="indicator">
@@ -85,199 +86,45 @@
     </div>
 </header>
 
-<div class="steps__wrapper hidden ">
+<div class="steps__wrapper hidden">
     <nav class="steps__menu">
         <ul class="steps__menu--items">
             <li><a class="steps__menu--item back" href="index.php?page=tutorial">Verlaat de tutorial</a></li>
             <li class="steps__menu--item tip">Vraag tips</li>
         </ul>
     </nav>
-    <section class="step step__1 hidden ">
+    <section class="step">
         <div class="step__wrapper">
             <div class="step__left">
-                <img src="../../assets/img/tutorial/step1/title.svg" alt="" class="step__title">
+                <img src="" alt="" class="step__title">
                 <p class="step__subtext">
-                    Elke maker begint zijn avontuur met schetsen. Zodat je zeker weet welke materialen en componenten je
-                    nodig
-                    hebt. Maak daarom een goede schets. Een top, front & side view zijn hierbij zeker niet overbodig.
+
                 </p>
-                <button id="1" class="button tutorial__button">Volgende</button>
+                <button id="btnStep0" class="button tutorial__button">Volgende</button>
             </div>
             <div class="step__right">
-                <img src="../../assets/img/tutorial/step1/image.svg" alt="" class="step__right--img">
+                <img src="" alt="" class="step__right--img">
             </div>
         </div>
         <div class="step__indicators">
             <div class="step__indicator ">
-                <p class="step__indicator--number step__indicator--active ">1</p>
+                <p class="step__indicator--number indicator0">1</p>
                 <p class="step__indicator--text">schets</p>
             </div>
             <div class="step__indicator">
-                <p class="step__indicator--number">2</p>
+                <p class="step__indicator--number indicator1">2</p>
                 <p class="step__indicator--text">body</p>
             </div>
             <div class="step__indicator">
-                <p class="step__indicator--number">3</p>
+                <p class="step__indicator--number indicator2">3</p>
                 <p class="step__indicator--text">aandrijving</p>
             </div>
             <div class="step__indicator">
-                <p class="step__indicator--number">4</p>
+                <p class="step__indicator--number indicator3">4</p>
                 <p class="step__indicator--text">wielen</p>
             </div>
             <div class="step__indicator">
-                <p class="step__indicator--number">5</p>
-                <p class="step__indicator--text">creativiteit</p>
-            </div>
-        </div>
-    </section>
-
-    <section class=" step__2 hidden">
-        <div class="step__wrapper">
-            <div class="step__left">
-                <img src="../../assets/img/tutorial/step2/title.svg" alt="" class="step__title">
-                <p class="step__subtext">
-                    Het eerste en ook het belangrijkste deel van de drag racewagen is de body. Dit deel houdt alles
-                    stevig bij elkaar. Je kan het in verschillende maten en figuren maken, zolang hij maar tegen een
-                    stootje kan. </p>
-                <button id="1" class="button tutorial__button">Volgende</button>
-            </div>
-            <div class="step__right">
-                <img src="../../assets/img/tutorial/step2/image.svg" alt="" class="step__right--img">
-            </div>
-        </div>
-        <div class="step__indicators">
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">1</p>
-                <p class="step__indicator--text">schets</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">2</p>
-                <p class="step__indicator--text">body</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number">3</p>
-                <p class="step__indicator--text">aandrijving</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number">4</p>
-                <p class="step__indicator--text">wielen</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number">5</p>
-                <p class="step__indicator--text">creativiteit</p>
-            </div>
-        </div>
-    </section>
-
-    <section class=" step__3 hidden">
-        <div class="step__wrapper">
-            <div class="step__left">
-                <img src="../../assets/img/tutorial/step3/title.svg" alt="" class="step__title">
-                <p class="step__subtext">
-                    Om je racewagen vooruit te helpen is een aandrijver een belangrijk onderdeel. Dit kan alles zijn.
-                    Een mixer, een motor van een speelgoed auto, boormachine. Het is belangrijk om hiervoor de juiste
-                    aandrijving te kiezen en goed vooraf te denken hoe je iets zal monteren met de meegeleverde klemmen.
-                </p>
-                <button id="1" class="button tutorial__button">Volgende</button>
-            </div>
-            <div class="step__right">
-                <img src="../../assets/img/tutorial/step3/image.svg" alt="" class="step__right--img">
-            </div>
-        </div>
-        <div class="step__indicators">
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">1</p>
-                <p class="step__indicator--text">schets</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">2</p>
-                <p class="step__indicator--text">body</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">3</p>
-                <p class="step__indicator--text">aandrijving</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number">4</p>
-                <p class="step__indicator--text">wielen</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number">5</p>
-                <p class="step__indicator--text">creativiteit</p>
-            </div>
-        </div>
-    </section>
-
-    <section class=" step__4 hidden">
-        <div class="step__wrapper">
-            <div class="step__left">
-                <img src="../../assets/img/tutorial/step4/title.svg" alt="" class="step__title">
-                <p class="step__subtext">
-                    Zonder de wielen kan je natuurlijk niet rijden. Bevestig de wielen aan je wielas met behulp van de
-                    meegeleverde connectie stukken. Draai de wielen stevig aan op de wielas om de speling zo klein
-                    mogelijk te houden. </p>
-                <button id="1" class="button tutorial__button">Volgende</button>
-            </div>
-            <div class="step__right">
-                <img src="../../assets/img/tutorial/step4/image.svg" alt="" class="step__right--img">
-            </div>
-        </div>
-        <div class="step__indicators">
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">1</p>
-                <p class="step__indicator--text">schets</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">2</p>
-                <p class="step__indicator--text">body</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">3</p>
-                <p class="step__indicator--text">aandrijving</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">4</p>
-                <p class="step__indicator--text">wielen</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number">5</p>
-                <p class="step__indicator--text">creativiteit</p>
-            </div>
-        </div>
-    </section>
-    <section class=" step__5 hidden">
-        <div class="step__wrapper">
-            <div class="step__left">
-                <img src="../../assets/img/tutorial/step5/title.svg" alt="" class="step__title">
-                <p class="step__subtext">
-                    Nu je het basis-model hebt, is het belangrijk om hier je eigen creativiteit aan te geven. Ga opzoek
-                    naar gekke dingen die je kan toevoegen aan de racewagen. Maar zorg hierbij ook dat je geen snelheid
-                    verliest! </p>
-                <button id="1" class="button tutorial__button">Volgende</button>
-            </div>
-            <div class="step__right">
-                <img src="../../assets/img/tutorial/step4/image.svg" alt="" class="step__right--img">
-            </div>
-        </div>
-        <div class="step__indicators">
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">1</p>
-                <p class="step__indicator--text">schets</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">2</p>
-                <p class="step__indicator--text">body</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">3</p>
-                <p class="step__indicator--text">aandrijving</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">4</p>
-                <p class="step__indicator--text">wielen</p>
-            </div>
-            <div class="step__indicator">
-                <p class="step__indicator--number step__indicator--active ">5</p>
+                <p class="step__indicator--number indicator4">5</p>
                 <p class="step__indicator--text">creativiteit</p>
             </div>
         </div>
