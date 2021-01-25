db6e955945505d4b2c7a5688b4833bbee00b5d92
Your Name HereEwout <ewout.verhamme@student.howest.be>
2021-01-25 20:33:18 +0100

    added back to home page

--- src/view/order/thankyou.php
+++ src/view/order/thankyou.php
@@ -1,4 +1,4 @@
 <div class="thankyou__wrapper">
     <h1 class="thankyou">Bedankt voor uw aankoop. </h1>
-    <a href="index.php" class="thankyou__home">Klik hier om terug naar home te gaan.</a>
+    <a href="index.php" class="thankyou__home"><span class="underline">Klik hier</span> om terug naar home te gaan.</a>
 </div>
