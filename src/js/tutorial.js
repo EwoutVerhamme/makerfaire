9c1ed9487c7567b915913a1054a389bdf4c38aac
Your Name HereEwout <ewout.verhamme@student.howest.be>
2021-01-25 16:47:08 +0100

    Added improvements for handdetection

--- src/js/tutorial.js
+++ src/js/tutorial.js
@@ -60,10 +60,10 @@
 
     const modelParams = {
       flipHorizontal: true, // flip e.g for video
-      imageScaleFactor: 0.7, // reduce input image size for gains in speed.
-      maxNumBoxes: 3, // maximum number of boxes to detect
+      imageScaleFactor: 1.0, // reduce input image size for gains in speed.
+      maxNumBoxes: 20, // maximum number of boxes to detect
       iouThreshold: 0.5, // ioU threshold for non-max suppression
-      scoreThreshold: 0.50, // confidence threshold for predictions.
+      scoreThreshold: 0.60, // confidence threshold for predictions.
     };
 
 
